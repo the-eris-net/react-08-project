2c9946846c98cdee03f2e62f792b23a1fe4f1592
the-eris-net <the.eris.net@gmail.com>
2025-10-19 03:02:25 +0900

    feat:flex-wrap으로 지정

--- chapter1/src/pages/App/index.jsx
+++ chapter1/src/pages/App/index.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import movieListData from '@/mocks/movieListData'
 import MovieCard from '@/components/MovieCard';
+import styles from '@/pages/App/App.module.css'
 
 function App() {
   const [movies, setMovies] = useState(movieListData.results);
 
   return (
-    <div>
+    <div className={styles.app}>
       {movies.map(movie=><MovieCard key={movie.id} movie={movie}/>)}
     </div>
   )
